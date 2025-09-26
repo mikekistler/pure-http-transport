70f3c794021b7dd8f817bb1d91678a1b6f9060c4
Mike Kistler <mikekistler@microsoft.com>
2025-09-26 08:20:43 -0500

    Add debug logging to the server

--- server/Program.cs
+++ server/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 using PureHttpTransport;
 using PureHttpTransport.OpenApiExtensions;
 
@@ -12,12 +14,27 @@ builder.Services.AddOpenApi(options =>
     options.AddOpenApiTransformers();
 });
 
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.RequestMethod
+                            | HttpLoggingFields.RequestPath
+                            // | HttpLoggingFields.RequestQuery
+                            | HttpLoggingFields.RequestHeaders
+                            // | HttpLoggingFields.RequestBody
+                            | HttpLoggingFields.ResponseStatusCode
+                            // | HttpLoggingFields.ResponseHeaders
+                            // | HttpLoggingFields.ResponseBody
+                            ;
+    logging.CombineLogs = false;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseHttpLogging();
 }
 
 app.UseHttpsRedirection();
